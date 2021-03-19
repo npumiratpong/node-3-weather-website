f13dba5841df57c9d2fde38c0367f15fe29da84c
Pumiratpong <Narin.Pumiratpong@thomsonreuters.com>
2021-03-19 19:54:14 +0700

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('Client side javascript file is loaded!')
+// console.log('Client side javascript file is loaded!')
 
 // fetch('http://localhost:3000/weather?address=!').then((response) => {
 //     response.json().then((data) => {
