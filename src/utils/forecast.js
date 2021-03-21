3ef484e798fa45153fb56a857718b2964ec528b9
Adam <rinpmp@gmail.com>
2021-03-21 10:38:53 +0700

    Add humid %

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,8 +8,10 @@ const forecast = (latitude, longtitude, callback) => {
         } else if(body.error) {
             callback('Unable to find location', undefined)
         }else {
-            callback(undefined,             
-                body.current.weather_descriptions[0] + ". It is currently " +body.current.temperature + " but it feels like " +  body.current.feelslike + " The humiduty is "+ body.current.humidity
+            console.log(body.current)  
+            callback(undefined,           
+                body.current.weather_descriptions[0] + ". It is currently " +body.current.temperature + " but it feels like " +  body.current.feelslike + 
+                " The humiduty is "+ body.current.humidity + "%."
             )
             
         }
